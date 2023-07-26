c33d86e50f48a4e37d895cc31ded1196b79e7854
sreedeviluminar <sreedevi@luminartechnolab.com>
2023-07-26 16:38:57 +0530

    commit

--- lib/splash2.dart
+++ lib/splash2.dart
@@ -7,20 +7,25 @@ void main() {
 class Splash2 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       body: Center(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-
-            Icon(Icons.cake,size: 80,color: Colors.green,),
-
-            Text("My Application",
+            // Icon(Icons.cake,size: 80,color: Colors.green,),
+            Image.asset(
+              "assets/icons/alience.png",
+            //  height: 100,
+            //  width: 100,
+            ),
+            Text(
+              "My Application",
               style: TextStyle(
                   fontSize: 30,
                   color: Colors.green,
-                  fontWeight:FontWeight.bold,
-                  fontStyle: FontStyle.italic
-              ),),
+                  fontWeight: FontWeight.bold,
+                  fontStyle: FontStyle.italic),
+            ),
           ],
         ),
       ),
